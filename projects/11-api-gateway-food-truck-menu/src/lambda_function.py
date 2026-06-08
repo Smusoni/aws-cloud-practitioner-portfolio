81071d3660dff70590a7538aac99f8a18d392944
Your Name <your-github-email>
2026-06-08 15:00:27 -0400

    Complete project 11: food truck menu API

---
+++ projects/11-api-gateway-food-truck-menu/src/lambda_function.py
@@ -0,0 +1,17 @@
+﻿import json
+
+def lambda_handler(event, context):
+    menu = {
+        "food_truck": "Terence Street Eats",
+        "items": [
+            {"name": "Birria Tacos", "price": "$12"},
+            {"name": "Loaded Fries", "price": "$9"},
+            {"name": "Grilled Chicken Bowl", "price": "$11"},
+            {"name": "Horchata", "price": "$4"}
+        ]
+    }
+    return {
+        "statusCode": 200,
+        "headers": {"Content-Type": "application/json"},
+        "body": json.dumps(menu)
+    }
